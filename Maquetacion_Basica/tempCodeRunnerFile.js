7e085f23b0b206f23f6a447507f5f05e8187df0a
fchecaruiz <fchecaruiz@gmail.com>
2024-10-29 19:20:26 +0100

    agencia_viajes.css

---
+++ Maquetacion_Basica/tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+// Asocia la función al evento 'click' del botón
+// document.getElementById("solicitar_informacion").addEventListener('click', function(event) {
+//     // Evita que el formulario se envíe y recargue la página
+//     event.preventDefault();
+//     // Llama a la función para recoger la información
+//     recogerInformacion();
+// });
